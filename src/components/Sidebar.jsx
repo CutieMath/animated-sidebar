6579a83df84bed46a885be52e34b70555dd895e6
Yuxin Ye <yuxin_ye@protonmail.com>
2022-11-03 16:56:12 +1100

    add animation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,14 +11,18 @@ const Sidebar = () => {
           className="cursor-pointer top-10 right-10 fixed text-3xl"
         />
       ) : (
-        <div className="flex justify-between top-0 right-0 fixed bg-pink-300 w-[35vw] h-full p-10">
-          <GiCardPickup
-            onClick={() => setIsOpen(false)}
-            className="cursor-pointer text-3xl"
-          />
-          <h2 className="text-2xl">This is the sidebar</h2>
-        </div>
+        <GiCardPickup
+          onClick={() => setIsOpen(false)}
+          className="top-10 right-10 fixed cursor-pointer text-3xl z-10"
+        />
       )}
+      <div
+        className={`ease-in duration-300 flex justify-between top-0 right-0 fixed bg-pink-300 w-[35vw] h-full p-10 ${
+          isOpen ? `translate-x-0` : `translate-x-full`
+        }`}
+      >
+        <h2 className="text-2xl">This is the sidebar</h2>
+      </div>
     </>
   );
 };
